31d25d984615459730e170f1b3f5e528115a7e5a
Pradeep Mouli <pmouli@mac.com>
2025-12-28 09:05:06 -0500

    feat: Implement buildV3 factory methods for Zod types

---
+++ src/ZodBuilder/v3.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+import { StringBuilder, NumberBuilder, BooleanBuilder, ObjectBuilder, ArrayBuilder, BigIntBuilder, DateBuilder } from './builders';
+
+const buildV3 = {
+  string: (params?: Parameters<typeof z.string>[0]) => new StringBuilder(params, 'v3'),
+  number: (params?: Parameters<typeof z.number>[0]) => new NumberBuilder(params, 'v3'),
+  boolean: (params?: Parameters<typeof z.boolean>[0]) => new BooleanBuilder(params, 'v3'),
+  object: (params?: Parameters<typeof z.object>[0]) => new ObjectBuilder(params, 'v3'),
+  array: (params?: Parameters<typeof z.array>[0]) => new ArrayBuilder(params, 'v3'),
+  bigint: (params?: Parameters<typeof z.bigint>[0]) => new BigIntBuilder(params, 'v3'),
+  date: (params?: Parameters<typeof z.date>[0]) => new DateBuilder(params, 'v3'),
+  // ...other factory methods...
+};
+
+export { buildV3 };
